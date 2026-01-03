130363b0539f0cf78c87601ca8ec4730eeecd65b
vnnicius <vinnicius109@gmail.com>
2026-01-03 11:05:13 -0300

    feat(core): add server config to settings file

--- src/core/config.py
+++ src/core/config.py
@@ -17,8 +17,24 @@ class ScraperAPIConfig(BaseSettings):
     COUNTRY_CODE: str = Field(default="br")
 
 
+class ServerConfig(BaseSettings):
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+        env_file_encoding="utf-8",
+        env_prefix="SERVER_",
+    )
+
+    TITLE: str = Field(default="Product Pulse Agent API")
+    DESCRIPTION: str = Field(default="Streaming API for Product Pulse Agent")
+    VERSION: str = Field(default="1.0.0")
+    FRONTEND_URL: str = Field(default="http://")
+    TOKEN: SecretStr
+
+
 class AppConfig(BaseModel):
     SCRAPER: ScraperAPIConfig = Field(default_factory=ScraperAPIConfig)
+    SERVER: ServerConfig = Field(default_factory=ServerConfig)
 
 
 app_config = AppConfig()
