945a446d02ebdbec2bac045b6bfd6e160de056a0
vnnicius <vinnicius109@gmail.com>
2026-01-03 10:04:05 -0300

    feat: add agent state

---
+++ src/agent/state.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from typing import Annotated
+
+from langgraph.graph.message import add_messages
+
+from ..core.models.amazon_product_details import ChatbotProductView
+
+
+@dataclass
+class State:
+    messages: Annotated[list, add_messages]
+    all_searches: Annotated[list[ChatbotProductView], add_messages]
+    last_search: list[ChatbotProductView] | None = field(default=None)
+    region: str | None = field(default=None)
