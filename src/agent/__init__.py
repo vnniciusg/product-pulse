c18b505588fb6dd53f089e050fc580dd86db5a28
vnnicius <vinnicius109@gmail.com>
2026-01-03 10:04:59 -0300

    feat: add the agent

---
+++ src/agent/__init__.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.agents.middleware import SummarizationMiddleware, ToolCallLimitMiddleware
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import InMemorySaver
+
+from .prompts import SYSTEM_PROMPT
+from .state import State
+from .tools.search_on_amazon import search_on_amazon
+
+load_dotenv()
+
+agent = create_agent(
+    model=init_chat_model("google_genai:gemini-3-flash-preview"),
+    system_prompt=SYSTEM_PROMPT,
+    tools=[search_on_amazon],
+    state_schema=State,
+    checkpointer=InMemorySaver(),
+    middleware=[
+        ToolCallLimitMiddleware(tool_name="search_on_amazon", run_limit=4),
+        SummarizationMiddleware(
+            model="google_genai:gemini-3-flash-preview", trigger={"tokens": 2048}
+        ),
+    ],
+)
